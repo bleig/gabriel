f3df719d24737abea31d64aaeb888877b25d3937
bleig <arrow7@outlook.es>
2018-03-12 18:50:22 -0600

    se agrego forms tabla clientes

---
+++ tabla_ble/clientesForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabla_ble
+{
+    public partial class clientesForm : Form
+    {
+        public clientesForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
